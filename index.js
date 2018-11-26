3eb79a6a0ec7928b0c20dd7683edabc48527281d
Cassidy Samelian <csamelian@gmail.com>
2018-11-26 23:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,20 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
+  
 if (string.toLowerCase() === string)
 {
   return 'I can\'t hear you!';
 }
 
-{
 if (string.toUpperCase() === string)
-return 'YES INDEED!';
+{
+  return 'YES INDEED!';
 }
 
-function sayHiToGrandma(string)
-{	
 if (string === "I love you, Grandma.")
+{
 return 'I love you, Grandma.';
+}
 }
 
