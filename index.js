2c36dc6056fb04ce50dfe974854dfdaee7740a14
Cassidy Samelian <csamelian@gmail.com>
2018-11-26 22:23:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) 
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  console.log("hello".toUpperCase())
+}
+
+function logWhisper(string)
+{
+  console.log("HELLO".toLowerCase())
+}
+
+function sayHiToGrandma(string)
+{
+console.log('I can\'t hear you!')
+return string.toLowerCase()
+}
+
+function sayHiToGrandma(string)
+{
+console.log('YES INDEED!')
+return string.toUpperCase()
+}
+
+function sayHiToGrandma(string)
+{	
+console.log('I love you, too.')
+return string
+}
+
